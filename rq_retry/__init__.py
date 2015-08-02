4e9dc1ac38b32e86985fc862515e8689d79463c4
Michael Keirnan <michael@keirnan.com>
2015-08-02 10:10:56 -0400

    Bump version: 0.1.0-dev → 0.1.0

--- rq_retry/__init__.py
+++ rq_retry/__init__.py
@@ -1,3 +1,3 @@
 from .worker import RetryWorker
 
-__version__ = '0.1.0-dev'
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rq-retry',
-    version='0.1.0-dev',
+    version='0.1.0',
     description='RQ retry support',
     long_description=__doc__,
     url='https://github.com/mgk/rq-retry/blob/master/README.md',
